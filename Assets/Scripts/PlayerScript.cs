dcca28b8da02c13c766d61cc820fad08f1b46735
SmallGuillotine <foodpunch@gmail.com>
2021-06-12 13:43:13 +0800

    Sprites + movement
    -Place holder sprites
    -Temp movement script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    public SpriteRenderer _sr;
+    public float moveMultiplier =5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInput();
+        SetSpriteDirection();
+    }
+    void MovementInput()
+    {
+        float x = Input.GetAxis("Horizontal")*moveMultiplier;
+        float y = Input.GetAxis("Vertical")*moveMultiplier;
+        _rb.velocity = new Vector2(x, y);
+    }
+    void SetSpriteDirection() //Smooth this later!
+    {
+        _sr.gameObject.transform.up = _rb.velocity;
+    }
+}
